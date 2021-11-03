8bd11137f946a8ea1d087934cd2ecccdcad1c0c7
Rodrigo Rech <rodrigorech92@gmail.com>
2021-11-03 17:18:44 -0300

    Feat: Context implemented

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Provider from './store/Provider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
